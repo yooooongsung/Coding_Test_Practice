91ffa3f2275ca4612c939d931bbe698c46fd0cc6
Yongsung <mirr001003@hanyang.ac.kr>
2024-03-11 15:22:59 +0900

    LV1-1

---
+++ Programmers/2024-03-11/dd.py
@@ -0,0 +1,9 @@
+def solution(x, n):
+    answer = []
+    for i in range(1,n+1):
+        answer.append(x*i)
+    return answer
+
+print(solution(10,12))
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/12954
