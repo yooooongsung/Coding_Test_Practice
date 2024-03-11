3ec478d0f10b35ef2d0e9e7a5b2e22e1256f1232
Yongsung <mirr001003@hanyang.ac.kr>
2024-03-11 17:11:47 +0900

    LV1-9

---
+++ Programmers/2024-03-11/LV1_서울에서 김서방 찾기.py
@@ -0,0 +1,8 @@
+def solution(seoul):
+    answer = seoul.index('Kim') 
+    #list : index / string : find
+    #arr.index('string',시작,끝) *find도 동일
+    
+    return "김서방은 " + str(answer) + "에 있다"
+
+#https://school.programmers.co.kr/learn/courses/30/lessons/12919
