8ec978dcefbca1eaba5dee8d19ed72edcfe50735
Yongsung <mirr001003@hanyang.ac.kr>
2024-03-13 11:07:03 +0900

    LV1-핸드폰 번호 가리기 문자열 슬라이싱

--- Programmers/2024-03-12/LV1-핸드폰 번호 가리기.py
+++ Programmers/2024-03-12/LV1-핸드폰 번호 가리기.py
@@ -1,7 +1,11 @@
 def solution(phone_number):
     answer = ''
-    rear = phone_number[-1:-5:-1]
     length = len(phone_number) - 4
-    for i in range(length):
-        answer = answer + '*'
-    return answer + rear
+    rear = phone_number[-4:]
+    answer = '*'* length + rear
+    return answer
+
+print(solution("01034678006")) #*******8006
+print(solution("1224")) #1224
+
+#https://school.programmers.co.kr/learn/courses/30/lessons/12948
