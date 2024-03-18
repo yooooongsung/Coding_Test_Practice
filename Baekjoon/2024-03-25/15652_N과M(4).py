a675994a834c62cbe2e2d5ec8d44945a7a6acfcd
Yongsung <mirr001003@hanyang.ac.kr>
2024-03-18 18:00:52 +0900

    주석

--- Baekjoon/2024-03-25/15652_N과M(4).py
+++ Baekjoon/2024-03-25/15652_N과M(4).py
@@ -9,7 +9,7 @@ def dfs(start): #start 파라미터
     for i in range(start,n+1):
         
         stack.append(i)
-        dfs(i) #stack에 푸시되는 같은 값을 재귀함수 아규먼트로 
+        dfs(i) #stack에 푸시되는 같은 값을 재귀함수 아규먼트로 -> i의 값이 바뀔 때 같이 바뀌기 때문에 크거나 같은 값으로 스택에 푸시 가능
         stack.pop()
 
 n,m = map(int,input().split())
