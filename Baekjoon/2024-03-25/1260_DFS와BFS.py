501839eb2f5b1f6b99a77d4c2926526af4bdae78
Yongsung <mirr001003@hanyang.ac.kr>
2024-03-19 11:40:50 +0900

    1260_setting

---
+++ Baekjoon/2024-03-25/1260_DFS와BFS.py
@@ -0,0 +1,9 @@
+def dfs():
+    return
+
+def bfs():
+    return
+
+n,m,v = map(int,input().split()) # 정점 : n, 간선 : m, 시작번호 : v
+for i in range(m):
+    a,b = map(int,input().split()) #간선의 갯수만큼의 입력값
