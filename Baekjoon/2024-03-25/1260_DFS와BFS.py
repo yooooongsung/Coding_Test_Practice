c6f4c16e8eef675a5db7a77ff910ca4e82c5b0e9
Yongsung <mirr001003@hanyang.ac.kr>
2024-03-19 15:24:57 +0900

    1260_DFS

--- Baekjoon/2024-03-25/1260_DFS와BFS.py
+++ Baekjoon/2024-03-25/1260_DFS와BFS.py
@@ -1,14 +1,34 @@
-def dfs():
+def dfs(v):
     
-    return
-
-def bfs():
-    return
-
-dic = [
+    visited[v] = True
+    print(v, end=' ')  # 방문한 정점 출력
+    for i in graph[v]:
+        if not visited[i]: #방문하지 않은 i에 대해서만 dfs 재귀 돌리기
+            dfs(i)
     
-]
+    
+          
+    
 
-n,m,v = map(int,input().split()) # 정점 : n, 간선 : m, 시작번호 : v
-for i in range(m):
-    a,b = map(int,input().split()) #간선의 갯수만큼의 입력값
+#def bfs():
+    #return
+
+
+
+n,m,v = map(int,input().split()) # 정점의 갯수 : n, 간선의 갯수 : m, 시작정점 번호 : v
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1) 
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+#for dfs
+for i in graph:
+    i.sort()
+
+dfs(v)
+
+#for bfs
+
