b0d1fd310c370c937da258b322cfb2329b7259cc
yongsung <mirr001003@hanyang.ac.kr>
2024-03-22 23:15:47 +0900

    n&m (2) 다시풀기

--- Baekjoon/2024-03-25/15650_N과M(2).py
+++ Baekjoon/2024-03-25/15650_N과M(2).py
@@ -1,25 +1,19 @@
-def dfs(start): #start 파라미터 
-    
-    
-    if len(stack) == m:
+def dfs(start):
 
-        print(' '.join(map(str,stack)))
+    if len(stack) == m:
+        print(' '.join(map(str,stack))) 
         return
     
-    
-    for i in range(start,n+1):
-        if visited[i]: 
-            continue
-        visited[i] = True  
-        stack.append(i) 
-        dfs(i+1) #아규먼트로 그 다음값부터 시작하도록함
-        stack.pop()
-        visited[i] = False    
-        
-    
+    for i in range(start,n+1): 
+        if not visited[i]: 
+            visited[i] = True
+            stack.append(i)
+            dfs(i) #첫 번째 시작 숫자보다 낮은 숫자로 시작하는 것을 방지. 
+            stack.pop()
+            visited[i] = False
 
-n,m = map(int,input().split())
-stack = []
+n, m = map(int,input().split())
 visited = [False] * (n+1)
-dfs(1)
+stack = []
 
+dfs(1)
