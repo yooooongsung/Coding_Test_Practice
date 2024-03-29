e8533c662d2e64c27c07f3c986b4dfcf9ad468a4
yongsung <mirr001003@hanyang.ac.kr>
2024-03-29 22:17:35 +0900

    2615_오목

--- Baekjoon/2024-04-01/2615_오목.py
+++ Baekjoon/2024-04-01/2615_오목.py
@@ -1,32 +1,41 @@
+import sys
+
 matrix = [list(map(int,input().split())) for _ in range(19)]
 dx = [-1,1,0,0,-1,-1,1,1]
 dy = [0,0,-1,1,-1,1,-1,1]
 
 
-def right(i,j,color,cnt):
-    if j+1 < 19:
-        nx = matrix[i][j+1]
-        if matrix[i][j] == nx:
-            right(i,j+1)
-            cnt += 1
-            if cnt == 5:
-                if color == 1:
-                    print(1)
-                else:
-                    print(2)
-        else:
-            return
+# → ↓ ↘ ↗
+dx = [0, 1, 1, -1]
+dy = [1, 0, 1, 1]
 
-for i in range(19):
-    for j in range(19):
-        if matrix[i][j] == 1:
-            cnt = 1
-            right(i,j,matrix[i][j],cnt)
-        elif matrix[i][j] == 2:
-            d = 1
-        else:
-            d = 1
+for x in range(19):
+    for y in range(19):
+        if matrix[x][y] != 0:
+            focus = matrix[x][y]
+            for i in range(4):
+                cnt = 1
+                nx = x + dx[i]
+                ny = y + dy[i]
 
+                while 0 <= nx < 19 and 0 <= ny < 19 and matrix[nx][ny] == focus:
+                    cnt += 1
+
+                    if cnt == 5:
+                        # 육목 체크
+                        if 0 <= x - dx[i] < 19 and 0 <= y - dy[i] < 19 and matrix[x - dx[i]][y - dy[i]] == focus:
+                            break
+                        if 0 <= nx + dx[i] < 19 and 0 <= ny + dy[i] < 19 and matrix[nx + dx[i]][ny + dy[i]] == focus:
+                            break
+                        # 육목이 아니면 성공한거니까 종료
+                        print(focus)
+                        print(x + 1, y + 1)
+                        sys.exit(0)
+
+                    nx += dx[i]
+                    ny += dy[i]
+
+print(0)
 
     
 
