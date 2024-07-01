b91638f40c9d1d22a2a751e711ea4c8912674bf5
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2024-07-01 13:38:00 +0900

    1992_쿼드트리

--- Baekjoon/2024-07-01/1992_쿼드트리.py
+++ Baekjoon/2024-07-01/1992_쿼드트리.py
@@ -1,4 +1,28 @@
 n = int(input())
 matrix = [list(map(int,input().strip())) for _ in range(n)]
 
-print(matrix)
+answer = []
+def recall(n,x,y):
+    value = matrix[x][y]
+    for i in range(x,n+x):
+        for j in range(y,n+y):
+            if value != matrix[i][j]:
+                answer.append('(')
+                recall(n//2,x,y)
+                recall(n//2,x,y+n//2)
+                recall(n//2,x+n//2,y)
+                recall(n//2,x+n//2,y+n//2)
+                answer.append(')')
+                return
+    answer.append(str(matrix[x][y]))
+    
+    
+
+recall(n,0,0)
+
+if len(answer) != 0:
+    result =''.join(answer)
+    print(result)
+else:
+    print(matrix[0][0])
+
