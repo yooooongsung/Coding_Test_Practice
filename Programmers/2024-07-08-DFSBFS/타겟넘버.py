0f5979ebbf3970ac50557c2a2fe58c556b26803e
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2024-07-03 19:29:23 +0900

    global 변수의 활용 / 함수 바깥에서 선언 후 작성하고 있는 함수 안에서 접근하기 위해서는 global 변수를 한 번 더 선언해줘야함

--- Programmers/2024-07-08-DFSBFS/타겟넘버.py
+++ Programmers/2024-07-08-DFSBFS/타겟넘버.py
@@ -12,3 +12,18 @@ def solution(numbers, target):
         return
     dfs(0,0)
     return answer
+
+# def solution(numbers, target):
+#     global answer
+#     answer = 0
+#     def dfs(i,total):
+#         global answer
+#         if (i==len(numbers)):
+#             if total==target:
+#                 answer+=1
+#             return
+#         dfs(i+1,total+numbers[i])    
+#         dfs(i+1,total-numbers[i])
+#         return
+#     dfs(0,0)
+#     return answer
