230d5a01db03044505fbdd375671c2f439682dc8
Yongsung <mirr001003@hanyang.ac.kr>
2024-07-30 10:26:06 +0900

    뭐가 다른거임?(집가서 지피티 돌려보겠음)

--- 삼성SDS 알고리즘 특강/07-29/3425_고스택.py
+++ 삼성SDS 알고리즘 특강/07-29/3425_고스택.py
@@ -1,93 +1,166 @@
-def process_operation(stack, operations):
-    for operation in operations:
-        if 'NUM' in operation:
-            _, num = operation.split()
-            stack.append(int(num))
-        elif not stack:
-            return "ERROR"
-        elif operation == 'DUP':
-            stack.append(stack[-1])
-        elif operation == 'POP':
-            stack.pop()
-        elif operation == 'INV':
-            stack[-1] = -stack[-1]
-        elif operation == 'SWP':
-            if len(stack) < 2:
-                return False
-            stack[-1], stack[-2] = stack[-2], stack[-1]
-        elif operation in ['ADD', 'SUB', 'MUL', 'DIV', 'MOD']:
-            if len(stack) < 2:
-                return False
-            b = stack.pop()
-            a = stack.pop()
-            if operation == 'ADD':
-                stack.append(a + b)
-            elif operation == 'SUB':
-                stack.append(a - b)
-            elif operation == 'MUL':
-                stack.append(a * b)
-            elif operation == 'DIV':
-                if b == 0:
-                    return 'ERROR'
-                # 나눗셈의 절댓값을 사용하여 계산
-                a_abs, b_abs = abs(a), abs(b)
-                result = a_abs // b_abs
-                if (a < 0) != (b < 0):  # 피연산자 중 하나가 음수일 경우
-                    result = -result
-                stack.append(result)
-            elif operation == 'MOD':
-                if b == 0:
-                    return 'ERROR'
-                # 나머지 계산
-                result = a % b
-                if a < 0:
-                    if b < 0:
-                        result = -(-a % -b)
-                    else:
-                        result = -(-a % b)
-                    print('11')
-                elif a > 0:
-                    if b < 0:
-                        result = a % -b
-                    else:
-                        result = a % -b
-                    print('22')
-                stack.append(result)
+import sys
 
-    return True
-
-def main():
-    operations = []
-    
-    while True:
-        x = input()
-        if x == '':
-            operations.clear()
-            continue
-        if x == 'QUIT':
-            break
-        if x == 'END':
-            n = int(input())
-            results = []
-            for _ in range(n):
-                stack = []
-                stack.append(int(input()))
-                
-                if process_operation(stack, operations):
-                    if len(stack) == 1:
-                        if stack[0] > 1000000000:
-                            results.append('ERROR')
-                        else:
-                            results.append(str(stack[0]))
-                    else:
-                        results.append('ERROR')
+input = sys.stdin.readline
+def process_operation(operations, first):
+        stack = [first]
+        for operation in operations:
+                if 'NUM' in operation:
+                        new = operation.split(' ')
+                        stack.append(int(new[1]))
+                elif not stack:
+                        return "ERROR"
+                elif operation == 'DUP':
+                        stack.append(stack[-1])
+                elif operation == 'POP':
+                        stack.pop()
+                elif operation == 'INV':
+                        stack[-1] *= -1
+                elif len(stack) == 1:
+                        return 'ERROR'    
+                elif operation == 'ADD':
+                        temp = stack.pop() + stack.pop()
+                        if abs(temp) > 10 ** 9:
+                                return 'ERROR'
+                        stack.append(temp)
+                elif operation == 'SWP':
+                        stack[-1], stack[-2] = stack[-2], stack[-1]   
+                elif operation == 'SUB':
+                        temp = -stack.pop() + stack.pop()
+                        if abs(temp) > 10 ** 9:
+                                return 'ERROR'
+                        stack.append(temp)                
+                elif operation == 'MUL':
+                        temp = stack.pop() * stack.pop()
+                        if abs(temp) > 10 ** 9:
+                                return 'ERROR'
+                        stack.append(temp)
+                elif operation == 'DIV':
+                        a , b = stack.pop(), stack.pop()
+                        if a == 0:
+                                return 'ERROR'
+                        temp = abs(b) // abs(a)
+                        if (a > 0 and b < 0) != (a < 0 and b > 0):  # 피연산자 중 하나가 음수일 경우
+                                temp = -temp
+                        if abs(temp) > 10 ** 9:
+                                return 'ERROR'
+                        stack.append(temp)
+                                
+                elif operation == 'MOD':# 나머지 계산
+                        a , b = stack.pop(), stack.pop()
+                        if a == 0:
+                                return 'ERROR'
+                        temp = abs(b) % abs(a)
+                        if a < 0:
+                                temp = -temp
+                        if abs(temp) > 10 ** 9:
+                                return 'ERROR'
+                        stack.append(temp)
                 else:
-                    results.append('ERROR')
-            print("\n".join(results))
-            print()
-            operations.clear()
-        else:
-            operations.append(x)
+                        return "ERROR"
 
-if __name__ == "__main__":
-    main()
+        if len(stack) == 1:
+            return stack[0]
+        return 'ERROR'
+
+
+
+    
+while True:
+        operations = []
+        while True:
+                operation = input().strip()
+                if operation == "QUIT":
+                        quit()
+                if operation == "END":
+                        break
+                operations.append(operation)
+        
+        n = int(input())
+        for _ in range(n):
+                first = int(input())
+                print(process_operation(operations,first))
+                
+        print()
+        input()
+                
+# import sys
+
+# input = sys.stdin.readline
+
+# def exec(commands, num):
+#     stack = [num]
+#     for cmd in commands:
+#         if cmd[:3] == "NUM":
+#             n = int(cmd[4:])
+#             stack.append(n)
+#         elif not stack:
+#             return "ERROR"
+#         elif cmd == "POP":
+#             stack.pop()
+#         elif cmd == "INV":
+#             stack[-1] *= -1
+#         elif cmd == "DUP":
+#             stack.append(stack[-1])
+#         elif len(stack) == 1:
+#             return "ERROR"
+#         elif cmd == "SWP":
+#             stack[-1], stack[-2] = stack[-2], stack[-1]
+#         elif cmd == "ADD":
+#             temp = stack.pop() + stack.pop()
+#             if abs(temp) > 10 ** 9:
+#                 return "ERROR"
+#             stack.append(temp)
+#         elif cmd == "SUB":
+#             temp = -stack.pop() + stack.pop()
+#             if abs(temp) > 10 ** 9:
+#                 return "ERROR"
+#             stack.append(temp)
+#         elif cmd == "MUL":
+#             temp = stack.pop() * stack.pop()
+#             if abs(temp) > 10 ** 9:
+#                 return "ERROR"
+#             stack.append(temp)
+#         elif cmd == "DIV":
+#             a, b = stack.pop(), stack.pop()
+#             if a == 0:
+#                 return "ERROR"
+#             temp = abs(b) // abs(a)
+#             if (a > 0 and b < 0) or (a < 0 and b > 0):
+#                 temp = -temp
+#             if abs(temp) > 10 ** 9:
+#                 return "ERROR"
+#             stack.append(temp)
+#         elif cmd == "MOD":
+#             a, b = stack.pop(), stack.pop()
+#             if a == 0:
+#                 return "ERROR"
+#             temp = abs(b) % abs(a)
+#             if b < 0:
+#                 temp = -temp
+#             if abs(temp) > 10 ** 9:
+#                 return "ERROR"
+#             stack.append(temp)
+#         else:
+#             return "ERROR"
+
+#     if len(stack) == 1:
+#         return stack[0]
+#     return 'ERROR'
+
+
+# while True:
+#     commands = []
+#     while True:
+#         cmd = input().strip()
+#         if cmd == "QUIT":
+#             quit()
+#         if cmd == "END":
+#             break
+#         commands.append(cmd)
+
+#     n = int(input())
+#     for _ in range(n):
+#         num = int(input())
+#         print(exec(commands, num))
+#     print()
+#     input()  # 줄바꿈 삭제
