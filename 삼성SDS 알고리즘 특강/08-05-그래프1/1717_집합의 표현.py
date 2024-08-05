1c4b90f0ffb7ea66cd8b1eec78ac858b6b259669
Yongsung <mirr001003@hanyang.ac.kr>
2024-08-05 13:09:00 +0900

    질문

--- 삼성SDS 알고리즘 특강/08-05-그래프1/1717_집합의 표현.py
+++ 삼성SDS 알고리즘 특강/08-05-그래프1/1717_집합의 표현.py
@@ -1,16 +1,16 @@
 import sys
 n, m = map(int,input().split())
-arr = {}
+dict = {}
 for _ in range(m):
     num, a, b = sys.stdin.readline().split()
     if num == '0': #합치기
-        if a in arr:
-            arr[a].append(b)
+        if a in dict: #key값이 딕셔너리에 있다면
+            dict[a].append(b) #배열로 value값을 넣어줌
         else:
-            arr[a] = [b]
+            dict[a] = [b] #key값이 딕셔너리에 없다면, 새로 추가
     elif num == '1':
-        check = arr.get(a)
-        if b in check:
-            print('YES')
-        else:
+        check = dict.get(a) #get함수로 key 값으로 value에 접근 >> check에 저장
+        if b in check: #b 값이 check에 있다면
+            print('YES') 
+        else: #없다면
             print('NO')
