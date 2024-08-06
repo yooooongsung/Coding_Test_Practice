a66ef473501a73e5eeeb1c4fd1633b9b10caaaca
Yongsung <mirr001003@hanyang.ac.kr>
2024-08-06 17:24:18 +0900

    이건 왜 안될까? / about exit()

--- Baekjoon/2024-08-05/12919_A와 B 2.py
+++ Baekjoon/2024-08-05/12919_A와 B 2.py
@@ -1,38 +1,16 @@
-#문자열 길이 차이 >> 할 수 있는 연산의 수
-#S가 A일때, T에 AA가 없다면 1번 연산은 어차피 못함
-#>> split을 활용해서, A끼리 뭉쳐있는 애들이 T에 있는지 확인
-#>> 없다면 2번째 연산밖에 못함
-s = input()
-t = input()
-round = len(t) - len(s)
+import sys
+s = list(input())
+t = list(input())
 
-def plusA(s,t):
+def change(t):
     if s == t:
-        return s
-    if s.count('A') < t.count('A'): #T의 A 갯수가 더 많을 때
-        x = s+'A'
-        if x in t: #A를 뒤에 추가한 결과가 T에 존재한다면
-            return plusA(x,t) #재귀돌리기
-        else: #A를 추가한 결과가 T에 존재하지 않다면
-            return reverseB(s,t)
-    else:
-        return reverseB(s,t)
-    
-def reverseB(s,t):
-    if s == t:
-        return s
-    if s.count('B') < t.count('B'):
-        x = s + 'B'
-        if x[::-1] in t:
-            return reverseB(x[::-1],t)
-        else:
-            return plusA(s,t)
-    else:
-        return plusA
-
-if plusA(s,t) == reverseB(s,t):
-    print(1)
-else:
-    print(0)
-
-
+        print(1)
+        sys.exit()
+    if len(t) == 0:
+        print(0)
+        sys.exit()
+    if t[-1] == 'A':
+        change(t[:-1])
+    if t[0] == 'B':
+        change(t[1:][::-1])
+change(t)
