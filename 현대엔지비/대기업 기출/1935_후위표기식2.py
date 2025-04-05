390453794134873104ae0456989a1a19b5c4b8df
yooooongsung <112863122+yooooongsung@users.noreply.github.com>
2025-04-05 16:06:57 +0900

    자릿수 출력 포맷

--- 현대엔지비/대기업 기출/1935_후위표기식2.py
+++ 현대엔지비/대기업 기출/1935_후위표기식2.py
@@ -3,35 +3,39 @@ import string
 input = sys.stdin.readline
 n = int(input())
 notation = input().strip()
+converted = []
 
 value = [int(input()) for i in range(n)]
 alphabet = list(string.ascii_uppercase[:n])
 dic = dict(zip(alphabet, value))
 
+for char in notation:
+    if char in dic: #key가 dic에 있다면
+        converted.append(dic[char]) # 알파벳이면 대응 숫자로
+    elif char == '*':
+        a = float(converted.pop())
+        b = float(converted.pop())
+        cal = a * b
+        converted.append(cal)
+
+    elif char == '+':
+        a = float(converted.pop())
+        b = float(converted.pop())
+        cal = a + b
+        converted.append(cal)
+
+    elif char == '-':
+        a = float(converted.pop())
+        b = float(converted.pop())
+        cal = b - a
+        converted.append(cal)
+
+    elif char == '/':
+        a = float(converted.pop())
+        b = float(converted.pop())
+        cal = b / a
+        converted.append(cal)
 
 
+print(f"{converted[0]:.2f}") #자릿수 출력 포맷
 
-# while i < len(notation):
-#     if notation[i] == '*':
-#         notation[i-2] = dic[notation[i-2]] * dic[notation[i-1]]
-#         notation[i-1] = ''
-#         notation[i] = ''
-#         i = 0
-#     elif notation[i] == '/':
-#         notation[i-2] = str(dic[notation[i-2]] / dic[notation[i-1]])
-#         notation[i-1] = ''
-#         notation[i] = ''
-#         i = 0
-#     elif notation[i] == '+':
-#         notation[i-2] = str(dic[notation[i-2]] + dic[notation[i-1]])
-#         notation[i-1] = ''
-#         notation[i] = ''
-#         i = 0
-#     elif notation[i] == '-':
-#         notation[i-2] = str(dic[notation[i-2]] - dic[notation[i-1]])
-#         notation[i-1] = ''
-#         notation[i] = ''
-#         i = 0
-#     i += 1
-
-# print(notation)
