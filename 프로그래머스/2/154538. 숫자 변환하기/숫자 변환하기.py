36122591b85e75fee842e8bf7aa3e6b8c2353690
윤용성 <112863122+yooooongsung@users.noreply.github.com>
2025-05-08 23:13:56 +0900

    [level 2] Title: 숫자 변환하기, Time: 570.53 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,26 @@
+from collections import deque
+def bfs(x,y,n):
+    q = deque()
+    visited = set()
+    dx = [2,3]
+    
+    q.append((x,0))
+    visited.add(x)
+    
+    while q:
+        a, cnt = q.popleft() #(10,0) / (15,1), (20,1), (30,1) / 
+        if a == y:
+            return cnt
+        for i in range(3):
+            if  i == 0:
+                nx = a + n
+            else:
+                nx = a * dx[i-1]
+            
+            if nx <= y and nx not in visited:
+                q.append((nx,cnt+1))
+                visited.add(nx)
+    return -1
+            
+def solution(x, y, n):
+    return bfs(x,y,n)
