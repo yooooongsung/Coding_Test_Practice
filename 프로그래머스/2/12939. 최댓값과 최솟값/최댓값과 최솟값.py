272afd42a7da3a09a1038d52e2b9b958100dd6cd
윤용성 <112863122+yooooongsung@users.noreply.github.com>
2025-10-29 16:38:02 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,11 +1,10 @@
 def solution(s):
     answer = ''
-    temp = s.split()
-    for i in range(len(temp)):
-        temp[i] = int(temp[i])
-    answer = str(min(temp)) + " " + str(max(temp))
+    s = s.split(' ')
+    arr = []
+    for i in s:
+        arr.append(int(i))
+    ma = max(arr)
+    mi = min(arr)
+    answer = str(mi) + ' ' + str(ma)
     return answer
-
-# def solution(s):
-#     temp = list(map(int, s.split()))  # split과 int 변환을 map으로 한 번에 처리
-#     return f"{min(temp)} {max(temp)}"  # f-string으로 간결하게 반환
