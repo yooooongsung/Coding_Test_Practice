4b26a3a00133c4aafe1cb345107612409f208613
윤용성 <112863122+yooooongsung@users.noreply.github.com>
2025-11-02 19:24:33 +0900

    [level 2] Title: 올바른 괄호, Time: 7.96 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,22 +1,14 @@
 def solution(s):
     answer = True
-    s_open = 0
-    s_close = 0
+    stack = []
     if s[0] == ')':
-            return False
+        return False
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        elif stack and stack[-1] == '(':
+                stack.pop()
+    if stack: 
+        return False
     else:
-        for i in range(len(s)):
-            if s_close > s_open:
-                return False
-            else:
-                if s[i] == '(':
-                    s_open += 1
-                else:
-                    s_close += 1
-    if s_open == s_close:
-        answer = True
-    else:
-        answer = False
-    
-
-    return answer
+        return True
