59113a703a917858830667fef5640024146607a7
yooooongsung <mirr001003@hanyang.ac.kr>
2026-02-09 11:47:09 +0900

    combination_6c2_재귀구현

--- combi.cpp
+++ combi.cpp
@@ -1,25 +1,31 @@
-#include <iostream>
 #include <bits/stdc++.h>
-
 using namespace std;
 
-void combi(int start, vector<int> b) {
+int k;
+int cnt = 0;
+vector<int> v = {1, 2, 3, 4, 5, 6};
+
+void print(vector<int> b) {
+    for(int i = 0; i < b.size(); i++) cout << v[b[i]] << " ";
+    cout << endl;
+}
+
+void combi(int start, vector<int>& b) {
     if(b.size() == k) {
+        cnt++;
+        print(b);
         return;
     }
-    for (int i = start + 1; i < n; i++) {
+    for (int i = start + 1; i < v.size(); i++) {
         b.push_back(i); // b[i] 가 아니라 i 로 한다. 중복 값이 있을 수 있기 때문에 index 기준으로 push하기.
         combi(i, b);
         b.pop_back();}
     }
 
 int main() {
-    vector<int> v = {2, 1, 3, 100, 200};
-    sort(v.begin(), v.end());
-    do{
-        for(int i = 0; i < v.size(); i++){
-            cout << v[i] << " ";
-        }
-        cout << endl;
-    }while(next_permutation(v.begin(), v.end()));
+    cin >> k;
+    vector<int> b;
+    combi(-1, b);
+    cout << cnt << endl;
+    return 0;
 }
