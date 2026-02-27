7c7ff83012e475c7e4ec8fbadf427bd5669e00e1
yooooongsung <mirr001003@hanyang.ac.kr>
2026-02-27 13:01:22 +0900

    stack ver 2

--- 큰돌/1주차/문제/3986_좋은 단어.cpp
+++ 큰돌/1주차/문제/3986_좋은 단어.cpp
@@ -1,22 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, res;
-bool flag;
-vector<char> st;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
+
     for(int i = 0; i < n; i++) {
         string s;
         cin >> s;
-        if(s.size() % 2 == 1) continue;
 
+        /* ver 1 */
+        vector<char> st;
+        for(char c : s) {
+            if(!st.empty() && st.back() == c) st.pop_back();
+            else st.push_back(c);
+        }
+        
+        if(st.empty()) res++;
+
+        /* ver 2
         st.push_back(s[0]); // A
         for(int j = 1; j < s.size(); j++) {
-            // 알파벳의 짝이 맞는지
-            // 교차하진 않은지, 즉 하나씩 나오는 경우라면 바로 탈락
             if(st.back() == s[j]) {
                 st.pop_back(); // A
                 continue;
@@ -24,7 +30,7 @@ int main() {
             st.push_back(s[j]); // A B
         }
         if(st.empty()) res++;
-        else st.clear();
+        else st.clear(); */
     }
     cout << res;
     
