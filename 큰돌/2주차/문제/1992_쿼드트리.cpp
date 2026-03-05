99908c9fd884fd8a2b315e295acb21bbe540c17b
yooooongsung <mirr001003@hanyang.ac.kr>
2026-03-05 15:47:52 +0900

    분할정복.. for문 return 조건 잘 챙기기

---
+++ 큰돌/2주차/문제/1992_쿼드트리.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+string ans;
+vector<vector<int>> matrix;
+vector<vector<int>> visited;
+
+void dfs(int n, int y, int x) {
+    int value = matrix[y][x];
+    for(int i = y; i < y + n; i++) {
+        for(int j = x; j < x + n; j++) {
+            if(value != matrix[i][j]) {
+                ans += '(';
+                dfs(n / 2, y, x);
+                dfs(n / 2, y, x + n / 2);
+                dfs(n / 2, y + n / 2, x);
+                dfs(n / 2, y + n / 2, x + n / 2);
+                ans += ')';
+                return;
+            }
+        }
+    }
+    ans += to_string(value);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    matrix.assign(n, vector<int>(n,0));
+    visited.assign(n, vector<int>(n,0));
+    string s;
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+        for(int j = 0; j < n; j++) {
+            matrix[i][j] = s[j] - '0';
+        }
+    }
+    dfs(n, 0, 0);
+    
+    cout << ans;
+    return 0;
+}
+
