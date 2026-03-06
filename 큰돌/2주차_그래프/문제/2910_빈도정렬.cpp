955b48c0e1df83f1e67a9a250d482cce751a7540
yooooongsung <mirr001003@hanyang.ac.kr>
2026-03-06 16:46:01 +0900

    comp, 정렬 비교 함수

---
+++ 큰돌/2주차_그래프/문제/2910_빈도정렬.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, c, ip;
+vector<int> v;
+unordered_map<int, int> m;
+vector<pair<int,int>> ans;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> c;
+    for(int i = 0; i < n; i++) {
+        cin >> ip;
+        if(find(v.begin(), v.end(), ip) == v.end()) {
+            v.push_back(ip); // 나온 순서를 담은 유니크한 값만 담긴 벡터
+        }
+        m[ip]++; // 빈도수 체크
+    }
+    for(const auto & it : m) {
+        ans.push_back({it.first, it.second});
+    }
+    sort(ans.begin(), ans.end(), 
+    [&](auto & a, auto & b) {
+        if(a.second != b.second) {
+            return a.second > b.second;
+        }
+        auto it_a = find(v.begin(), v.end(), a.first);
+        auto it_b = find(v.begin(), v.end(), b.first);
+        return it_a < it_b;
+    });
+    for(const auto & it : ans) {
+        for(int i = 0; i < it.second; i++) {
+            cout << it.first << " ";
+        }
+    }
+
+    return 0;
+}
