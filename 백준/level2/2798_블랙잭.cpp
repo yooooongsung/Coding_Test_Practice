305304c136bec2d8983dfd3534cef2387f70d595
yongsung <mirr001003@hanyang.ac.kr>
2026-03-16 02:21:04 +0900

    백트래킹 스탠다드

---
+++ 백준/level2/2798_블랙잭.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int n, m;
+vector<int> v;
+vector<int> res;
+int ans;
+void dfs(int idx) {
+    if(res.size() == 3) {
+        int temp = 0;
+        for(int i = 0; i < 3; i++) {
+            temp += res[i];
+        }
+        if(temp <=  m) ans = max(ans, temp);
+        return;
+    }
+    if(idx == n) return;
+    res.push_back(v[idx]);
+    dfs(idx + 1);
+    res.pop_back();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        int ip; cin >> ip;
+        v.push_back(ip);
+    }
+    dfs(0);
+    // for(int i = 0; i < n; i++) {
+    //     dfs(i);
+    // }
+    cout << ans;
+    
+    return 0;
+}
