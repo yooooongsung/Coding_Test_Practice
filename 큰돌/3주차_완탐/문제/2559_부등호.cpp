f09e41803c3d62306707270b518a8b147d5d8047
yooooongsung <mirr001003@hanyang.ac.kr>
2026-04-01 14:16:23 +0900

    문자열 비교시 min, max 잡기 / 조건문 단순하게 / 방문배열 초기화

---
+++ 큰돌/3주차_완탐/문제/2559_부등호.cpp
@@ -0,0 +1,46 @@
+    #include <bits/stdc++.h>
+    using namespace std; 
+    int k;
+    string _min = "9999999999", _max = "";
+    char arr[9];
+    int visited[10];
+    string s;
+    void dfs(int num, int idx) {
+        s.push_back(num + '0');
+        visited[num] = 1;
+        if(s.size() == k + 1) {
+            if (s < _min) _min = s;
+            if (s > _max) _max = s;
+        }
+        
+        for(int j = 0; j <= 9; j++) {
+            if(visited[j] == 0) {
+                if(arr[idx] == '<') {
+                    if(num < j) dfs(j, idx + 1);
+                }
+                else if(arr[idx] == '>') {
+                    if(num > j) dfs(j, idx + 1);
+                }
+            }
+        }
+
+        s.pop_back();
+        visited[num] = 0;
+        return;
+    }
+    int main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
+        cin >> k;
+        for(int i = 0; i < k; i++) {
+            cin >> arr[i];
+        }
+        for(int i = 0; i <= 9; i++) {
+            s = "";
+            memset(visited, 0, sizeof(visited));
+            dfs(i, 0);
+        }
+        cout << _max << "\n" << _min;
+        
+        return 0;
+    }
